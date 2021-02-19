a955968bbc493663236ceab9dea3686619e0d122
Yufan You <ouuansteve@gmail.com>
2021-02-19 21:48:16 +0800

    fix: handle errors

--- src/main.js
+++ src/main.js
@@ -3,24 +3,28 @@ const github = require('@actions/github');
 const { Octokit } = require('@octokit/rest');
 
 (async () => {
-  const token = core.getInput('token') || process.env.GITHUB_TOKEN;
-  const octokit = new Octokit({ auth: `token ${token}` });
+  try {
+    const token = core.getInput('token') || process.env.GITHUB_TOKEN;
+    const octokit = new Octokit({ auth: `token ${token}` });
 
-  const { owner, repo } = github.context.repo;
+    const { owner, repo } = github.context.repo;
 
-  const { stargazers_count: stars, forks_count: forks } = (await octokit.repos.get({
-    owner,
-    repo,
-  })).data;
+    const { stargazers_count: stars, forks_count: forks } = (await octokit.repos.get({
+      owner,
+      repo,
+    })).data;
 
-  const descriptionTemplate = core.getInput('template') || '<star-fork>';
-  const description = descriptionTemplate.replace('<star-fork>', `This repo has ${stars} star${stars > 1 ? 's' : ''} ${forks} fork${forks > 1 ? 's' : ''}`)
-    .replace('<star>', stars.toString())
-    .replace('<fork>', forks.toString());
+    const descriptionTemplate = core.getInput('template') || '<star-fork>';
+    const description = descriptionTemplate.replace('<star-fork>', `This repo has ${stars} star${stars > 1 ? 's' : ''} ${forks} fork${forks > 1 ? 's' : ''}`)
+      .replace('<star>', stars.toString())
+      .replace('<fork>', forks.toString());
 
-  await octokit.repos.update({
-    owner,
-    repo,
-    description,
-  });
+    await octokit.repos.update({
+      owner,
+      repo,
+      description,
+    });
+  } catch (error) {
+    core.setFailed(error);
+  }
 })();
